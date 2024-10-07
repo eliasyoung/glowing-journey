b22d015de0ace58c7e8147d8b429b3480cac8abc
eliasyoung <warlockD@126.com>
2024-10-08 00:57:24 +0800

    Add parsing token for auth middleware

--- project-athena-server/src/web/middleware_auth.rs
+++ project-athena-server/src/web/middleware_auth.rs
@@ -2,15 +2,29 @@ use axum::body::Body;
 use axum::http::Request;
 use axum::middleware::Next;
 use axum::response::Response;
+use lazy_regex::regex_captures;
 use tower_cookies::Cookies;
 
 use crate::web::AUTH_TOKEN;
 use crate::{Error, Result};
 
-pub async fn middleware_require_auth(cookies: Cookies, req: Request<Body>, next: Next) -> Result<Response> {
+pub async fn middleware_require_auth(
+    cookies: Cookies,
+    req: Request<Body>,
+    next: Next,
+) -> Result<Response> {
     let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
 
     auth_token.ok_or(Error::AuthFailNoAuthTokenCookie)?;
 
     Ok(next.run(req).await)
+}
+
+fn parse_token(token: String) -> Result<(u64, String, String)> {
+    let (_whole, user_id, exp, sign) = regex_captures!(r#"^user-(\d+)\.(.+)\.(.+)"#, &token)
+        .ok_or(Error::AuthFailTokenWrongFormat)?;
+
+    let user_id: u64 = user_id.parse().map_err(|_| Error::AuthFailTokenWrongFormat)?;
+
+    Ok((user_id, exp.to_string(), exp.to_string()))
 }
